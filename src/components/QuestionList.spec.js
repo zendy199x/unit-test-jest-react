6a8354f0ff8d12073fd0b5df7404e2d85ebae705
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-12-23 17:36:05 +0700

    skipping and Isolating tests

--- src/components/QuestionList.spec.js
+++ src/components/QuestionList.spec.js
@@ -15,11 +15,11 @@ describe (`The question list`, () => {
         console.log('After all !')
     })
 
-    it (`should display a list of items`, () => {
+    it.only(`should display a list of items`, () => {
         expect(1 + 1).toEqual(2);
     })
 
-    it (`should be the meaning of life`, () => {
-        expect(1 + 1).toEqual(2);
+    it.skip(`should be the meaning of life`, () => {
+        expect(1 + 1).toEqual(3);
     })
 })
