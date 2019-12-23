b78f243787aa728ce81a688ffe58b06de6534645
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-12-23 17:29:07 +0700

    setup and teardown demo

--- src/components/QuestionList.spec.js
+++ src/components/QuestionList.spec.js
@@ -1,5 +1,25 @@
 describe (`The question list`, () => {
+    beforeEach(() => {
+        console.log('Before each !')
+    })
+
+    beforeAll(() => {
+        console.log('Before all !')
+    })
+
+    afterEach(() => {
+        console.log('After each !')
+    })
+
+    afterAll(() => {
+        console.log('After all !')
+    })
+
     it (`should display a list of items`, () => {
-        expect(1 + 1).toEqual(2)
+        expect(1 + 1).toEqual(2);
+    })
+
+    it (`should be the meaning of life`, () => {
+        expect(1 + 1).toEqual(2);
     })
 })
