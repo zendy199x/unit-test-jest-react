663492b69463026b8199270e38a579b8e22206a0
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-12-24 14:23:01 +0700

    react redux display testing demo

--- src/components/__tests__/QuestionDetail.js
+++ src/components/__tests__/QuestionDetail.js
@@ -1,25 +1,43 @@
-import { mapStateToProps } from '../QuestionDetail';
+import QuestionDetail, {
+    mapStateToProps,
+    QuestionDetailDisplay
+} from "../QuestionDetail";
+import renderer from "react-test-renderer";
+import React from "react";
+
 describe(`The Question Detail Component`, () => {
     describe(`The Container Element`, () => {
         describe(`mapStateToProps`, () => {
-            it (`should map the state to props correctly`, () => {
+            it("should map the state to props correctly", () => {
                 const sampleQuestion = {
                     question_id: 42,
-                    body: `Space is big`
-                }
-                const appState = {
+                    body: "Space is big"
+                };
+                const appStore = {
                     questions: [sampleQuestion]
                 };
                 const ownProps = {
-                    question_id: 42,
+                    question_id: 42
                 };
-                const componentState = mapStateToProps(appState, ownProps);
+                const componentState = mapStateToProps(appStore, ownProps);
                 console.log(componentState);
-                expect(componentState).toEqual(sampleQuestion)
-            })
-        })
-    })
-    it(`Should not regress`, () => {
-        
-    })
-})
+                expect(componentState).toEqual(sampleQuestion);
+            });
+        });
+    });
+
+    describe(`The display element`, () => {
+        it(`Should not regress`, () => {
+            const tree = renderer.create(
+                <QuestionDetailDisplay
+                    title="THANKS"
+                    body="43"
+                    answer_count={0}
+                    tags={[`abcde`]}
+                />
+            );
+
+            expect(tree.toJSON()).toMatchSnapshot();
+        });
+    });
+});
